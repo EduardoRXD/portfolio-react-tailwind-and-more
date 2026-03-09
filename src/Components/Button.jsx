7003754c0779bca1bc5cd760db190462a792f2e2
Eduardo Rodrigues <dudu1005@hotmail.com>
2026-03-09 08:14:07 -0300

    commit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,5 @@
-function Button({text, paddingX, paddingY}) {
-    if (paddingX && paddingY) {
-        return <button class={`bg-[rgb(89,17,148)] text-[rgb(229,165,238)] border-0 rounded-4xl cursor-pointer font-semibold text-lg ${paddingX} ${paddingY} hover:shadow-[0_0_30px_rgb(229,165,238)] hover:scale-110 ease-in-out duration-200`}>{text}</button>
-    } else {
-
-    }
+function Button({ text, paddingX, paddingY, BorderRadius}) {
+    return <button class={`bg-[rgb(89,17,148)] text-[rgb(229,165,238)] border-0 ${BorderRadius ? BorderRadius : "rounded-4xl"} cursor-pointer font-semibold text-lg ${paddingX ? paddingX: "px-10"} ${paddingY ? paddingY: "py-2"} hover:shadow-[0_0_30px_rgb(229,165,238)] hover:scale-110 ease-in-out duration-200`}>{text}</button>
 }
 
 export default Button
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import SocialMediaBtn from "../Components/SocialMediaBtn"
 import TextButton from "../Components/textButton"
 import ProjectsBtn from "../Components/ProjectsBtn"
+import Button from "../Components/Button"
 
 function Home() {
     const [emojiDivs, setEmojiDivs] = useState([]);
@@ -49,9 +50,9 @@ function Home() {
                                 <p class="text-justify my-5 text-[22px] leading-7">In 2024 I spent some time between the end of my first high school year, and new year's eve working at Eskimó Sorvetes as a sales assistant, and after finishing high school, I enrolled at IFRS-Rolante exploring the field of software development</p>
                                 <p class="text-justify my-5 text-[22px] leading-7">Currently, I'm looking for job opportunities in the IT industry, preferably close where I live.</p>
                                 <div class="flex ">
-                                    <SocialMediaBtn link={"https://www.linkedin.com/in/eduardo-rodrigues-3802ba330/"} icon={"bi bi-linkedin"}/>
-                                    <SocialMediaBtn link={"https://github.com/EduardoRXD"} icon={"bi bi-github"}/>
-                                    <SocialMediaBtn link={"https://judge.beecrowd.com/en/profile/1096694"} icon={"/beecrowd.png"}/>
+                                    <SocialMediaBtn link={"https://www.linkedin.com/in/eduardo-rodrigues-3802ba330/"} icon={"bi bi-linkedin"} />
+                                    <SocialMediaBtn link={"https://github.com/EduardoRXD"} icon={"bi bi-github"} />
+                                    <SocialMediaBtn link={"https://judge.beecrowd.com/en/profile/1096694"} icon={"/beecrowd.png"} />
                                 </div>
                             </div>
                         </Flex>
@@ -61,12 +62,46 @@ function Home() {
                 <section class="Projects shadow-[0_0_30px_rgba(141,63,151,0.308)] py-15 px-[4%] ">
                     <Interface>
                         <h2 class="font-bold text-[50px] text-center">Projects</h2>
-                        <div class="text-right py-5"><TextButton text={"View All"} fontSize={"25px"}/></div>
-                        <ProjectsBtn/>
+                        <div class="text-right py-5"><TextButton text={"View All"} fontSize={"25px"} /></div>
+                        <ProjectsBtn />
                     </Interface>
                 </section>
 
-                <section class="About Me"></section>
+                <section class="Contact mt-27.5 ">
+                    <Interface>
+                        <h2 class="font-bold text-[50px] text-center mb-8">Contact Me</h2>
+
+                        <form action="https://formsubmit.co/eduardorodgs00@gmail.com" method="POST" class="max-w-125 m-auto flex justify-center flex-col gap-2.5">
+                            <input type="text" name="name" id="" placeholder="Name:" required class="w-full border-0 px-5 py-4 bg-[rgba(190,190,190,0.767)] outline-0 text-base rounded-2xl"/>
+                            <input type="text" name="email" id="" placeholder="Email:" required class="w-full border-0 px-5 py-4 bg-[rgba(190,190,190,0.767)] outline-0 text-base rounded-2xl"/>
+                            <input type="text" name="phone" id="" placeholder="Phone number(optional):" class="w-full border-0 px-5 py-4 bg-[rgba(190,190,190,0.767)] outline-0 text-base rounded-2xl"/>
+                            <textarea name="message" id="" placeholder="Message:" required class="h-30 m-h-50 resize-none w-full border-0 px-5 py-4 bg-[rgba(190,190,190,0.767)] outline-0 text-base rounded-2xl"></textarea>
+                            <div class="submit text-center mt-4 mb-20">
+                                <Button text={"submit"} paddingX={"w-30"} paddingY={"h-15"} BorderRadius={"rounded-[15px]"}/>
+                            </div>
+                        </form>
+                    </Interface>
+                </section>
+                
+                <footer class="shadow-[0_0_30px_rgba(141,63,151,0.308)] pb-15 px-[4%]">
+                    <Interface>
+                        <div class="pb-5">
+                            <Flex> 
+                                <div class="logo">
+                                    <a href=""><img src="/Logo.png" alt="" /></a>
+                                </div>
+                                <Flex gap={"0"}>
+                                    <SocialMediaBtn link={"https://www.linkedin.com/in/eduardo-rodrigues-3802ba330/"} icon={"bi bi-linkedin"} />
+                                    <SocialMediaBtn link={"https://github.com/EduardoRXD"} icon={"bi bi-github"} />
+                                    <SocialMediaBtn link={"https://judge.beecrowd.com/en/profile/1096694"} icon={"/beecrowd.png"} />
+                                </Flex>
+                            </Flex>
+                        </div>
+                        <div class="border-solid border-t-2 border-[rgb(89,17,148)] py-5">
+                            <p><i class="bi bi-envelope-fill text-[rgb(89,17,148)] text-3xl "></i> <a class="text-[20px]" href="mailto:eduardorodgs00@gmail.com">eduardorodgs00@gmail.com</a></p>
+                        </div>
+                    </Interface>
+                </footer>
             </main>
         </>
     )
